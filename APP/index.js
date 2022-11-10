93e3a8d9d9084cc38f92b842f2a0ec573a5aa686
Fernando21853008 <114548700+Fernando21953008@users.noreply.github.com>
2022-11-09 18:51:16 -0600

    Add files via upload

---
+++ APP/index.js
@@ -0,0 +1,21 @@
+Suma()
+
+let variable = "Hola mundo";
+alert(variable); 
+console.log("Hola mundo");  
+
+function suma()
+{
+    let nombre= "Fernando";
+    let variable = "le = "Hola  + nombre";
+    alert(variable)
+}
+
+function suma2()
+{
+    var numero1,numero2,sumanumeros;
+    numero1 = parseFloat(prompt(numero1));
+    numero2 = parseFLoat(prompt(numero2));
+    sumanumeros = numero1 + numero2;
+    alert(sumanumeros);
+}
