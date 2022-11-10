1aa285f30205407acfb42e16ba9ed50ebd218b08
Fernando21853008 <114548700+Fernando21953008@users.noreply.github.com>
2022-11-09 18:45:30 -0600

    Add files via upload

---
+++ APP/index2.js
@@ -0,0 +1,21 @@
+
+//let numero = parseFloat(prompt("Dame un número"));
+//let numero2 = parseFloat(prompt("Dame otro número"));
+
+//console.log( typeof (numero + numero2));
+
+//document.write (numero + numero2);
+
+let numeros = [3,"Fernando", true,false, "Hola"];
+
+//Los array tienen un indice y comienza en 0
+
+for (let i = 0; i < numeros.length; i++) {
+    
+    console.log(numeros[i]);
+
+    document.write(numeros[i]+ "<br>";
+    
+}
+// El comando for es un bucle
+// Scope es el alcanze 
