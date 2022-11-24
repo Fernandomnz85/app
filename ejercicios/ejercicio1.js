aa253a9bccce9a85453365bff80849bc1cfc4311
Fernando21853008 <114548700+Fernando21953008@users.noreply.github.com>
2022-11-23 21:51:58 -0600

    Delete ejercicio1.js

--- ejercicios/ejercicio1.js
+++
@@ -1,5 +0,0 @@
-function numeroCuadrado() {
-let numeroCuadrado = parseInt (document.getElementById("numero").value);
-let formula= (numeroCuadrado*numeroCuadrado)
-return document.write (formula)
-}
