1859c68571b1fcda79016e1c9e14359635dd3440
Fernando21853008 <114548700+Fernando21953008@users.noreply.github.com>
2022-11-23 21:18:18 -0600

    Add files via upload

---
+++ ejercicios/ejercicio1.js
@@ -0,0 +1,5 @@
+function numeroCuadrado() {
+let numeroCuadrado = parseInt (document.getElementById("numero").value);
+let formula= (numeroCuadrado*numeroCuadrado)
+return document.write (formula)
+}
